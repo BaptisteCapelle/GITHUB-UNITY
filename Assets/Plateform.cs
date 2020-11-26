abe5881e411f4d1c8d0f9df2eb5078e3d94948fd
LAPTOP-FIUKPBFK\Baptiste.C <baptiste.capelle@lecole-ldlc.com>
2020-11-26 14:48:00 +0100

    rajout de 2 plateforme qui font des aller retours + couleurs plateformes

---
+++ Assets/Plateform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plateform : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 positionDeDepart;
+    float Vitesse;
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    Vector3 direction = new Vector3(0f, 0f, 1f);
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        float speed = 2f;
+        if (currentPosition.z > 5)
+        {
+            direction = new Vector3(0F, 0F, -1f);
+        }
+        if (currentPosition.z < 1)
+        {
+            direction = new Vector3(0F, 0F, 1f);
+        }
+
+        Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
+
+        rigidbody.MovePosition(newPosition);
+
+        
+    }
+}
