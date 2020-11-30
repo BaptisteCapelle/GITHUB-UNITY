954fa366168152c92ed908f5dcf75185f62dfb4e
LAPTOP-FIUKPBFK\Baptiste.C <baptiste.capelle@lecole-ldlc.com>
2020-11-30 13:17:50 +0100

    Rajout plateforme noire, projectiles, cube bleu, voiture, terrain

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    GameObject point;
+    Vector3 newposition;
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        newposition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -1f)
+        {
+            transform.position = newposition;
+        }
+    }
+}
